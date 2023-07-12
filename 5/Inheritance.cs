dfdcaae7ba947af932304b2799d081a6a6a1de9c
Slava <kuncovs19@gmail.com>
2023-07-13 00:50:50 +0300

    add 5: did 1), 2), 3) full tasks

---
+++ 5/Inheritance.cs
@@ -0,0 +1,211 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Xml.Linq;
+
+namespace _4
+{
+    // ______________ Перечесление ______________
+    public enum ProductCategorty
+    {
+        Device,
+        PrinterDevice,
+        Scanner,
+        Computer,
+        Tablet
+    }
+
+
+    // ______________ Структура ______________
+    public struct ProductInfo
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public ProductCategorty Category { get; set; }
+    }
+
+
+    // ______________ 1) Интерфейсы ______________
+    public interface ITech
+    {
+        string Name { get; set; }
+        decimal Price { get; set; }
+        string ToString();
+    }
+
+
+    // ______________ 4) Одноименные интерфкйс Clone  ______________
+    public interface IClone
+    {
+        string Clone();
+    }
+
+
+    // ______________ 2) Абстрактный класс ______________
+    public abstract class Products
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public ProductCategorty Category { get; set; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+    // ______________ 1) Классы ______________
+    public class Device : Products, ITech // может тоже abstract class ????????
+    {
+        public string? Manufacturer { get; set; } // производитель
+
+        // ______________ Переопределение методов от Object ______________
+        public override bool Equals(object? obj)
+        {
+            return obj is Products products &&
+                   Name == products.Name &&
+                   Price == products.Price;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}, Manufacturer: {Manufacturer}, Category: {Category}";
+        }
+
+        // заменнем методом ToString() по заданию 6)
+        //______________ Виртуальный метод ______________
+        //public virtual string GetInfo() // перегрузили виртуальный метод
+        //{
+        //    return $"Product: {Name}, Price: {Price}, Manufacturer: {Manufacturer}";
+        //}
+    }
+    public class PrinterDevice : Device, ITech
+    {
+        public bool IsColor { get; set; }
+        public int PrintSpeed { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}, Manufacturer: {Manufacturer}, IsColor: {IsColor}, PrintSpped: {PrintSpeed}, Category: {Category}";
+        }
+    }
+    public class Scanner : Device, ITech
+    {
+        public int ScanResolution { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}, Manufacturer: {Manufacturer}, ScanResolution: {ScanResolution}, Category: {Category}";
+        }
+    }
+    public class Computer : Device, ITech
+    {
+        public string Processor { get; set; }
+        public string GraphicsCard { get; set; }
+
+        // ______________ 3) Запрет от sealed на переопределение ______________
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}, Manufacturer: {Manufacturer}, Processor: {Processor}, GraphicsCard: {GraphicsCard}, Category: {Category}";
+        }
+    }
+    public class Tablet : Device, ITech
+    {
+        public int ScreenSize { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}, Manufacturer: {Manufacturer}, Screen Size: {ScreenSize}, Category: {Category}";
+        }
+    }
+
+
+    // ______________ 3) Запрет от sealed на переопределение ______________
+    public class Laptop : Computer, ITech
+    {
+        //public override string GetInfo()
+        //{
+        //    return "123";
+        //}
+    }
+
+
+    // ______________ 7) Класс Printer ______________
+    partial class Printer
+    {
+        public static void IAmPrinting(Products products)
+        {
+            Console.WriteLine("Origin Printer:");
+            Console.WriteLine("    " + products.ToString() + "\n");
+        }
+    }
+
+
+    // ______________ 4) Одноименные класс Clone ______________
+    public abstract class ClassCloneAbstact
+    {
+        public abstract string Clone();
+    }
+
+    class InheritanceClone : ClassCloneAbstact, IClone
+    {
+        public override string Clone()
+        {
+            return "ClassClone void";
+        }
+
+        string IClone.Clone()
+        {
+            return "IClone void";
+        }
+    }
+
+
+    // ______________ Функции ______________
+    class Inheritance
+    {
+        static void Main()
+        {
+            Console.WriteLine("\n______________ 1 ______________\n");
+
+            Device dev = new() { Name = "device", Price = 10000, Manufacturer = "China", Category = ProductCategorty.Device };
+
+            Printer.IAmPrinting(dev);
+
+            Computer computer = new() { Name = "my desktop", Price = 3500, Manufacturer = "China", GraphicsCard = "1650", Processor = "ryzen 5", Category = ProductCategorty.Computer };
+
+            Printer.IAmPrintingPartial(computer);
+
+            Console.WriteLine("\n______________ 3 ______________\n");
+
+            Laboratory lab = new Laboratory();
+
+            lab.Add(new Device { Name = "device", Price = 1, Manufacturer = "Chinaaaaaaa", Category = ProductCategorty.Device });
+            lab.Add(new Device { Name = "device", Price = 1000, Manufacturer = "Chinaaaaaaa", Category = ProductCategorty.Device });
+            lab.Add(new Device { Name = "device", Price = 15, Manufacturer = "Chinaaaaaaa", Category = ProductCategorty.Device });
+            lab.Add(new Computer { Name = "computer", Price = 3000, Manufacturer = "ccc", Category = ProductCategorty.Computer });
+            lab.Add(new Computer { Name = "computer2", Price = 3300, Manufacturer = "ccc2", Category = ProductCategorty.Computer });
+            lab.Add(new Computer { Name = "computer2", Price = 3300, Manufacturer = "ccc2", Category = ProductCategorty.Computer });
+            lab.Add(new Scanner { Name = "scanner", Price = 89, Manufacturer = "Беларусь", ScanResolution = 1000, Category = ProductCategorty.Scanner });
+
+
+            Console.WriteLine("\n-----------");
+
+            lab.SortByPrice();
+
+
+            Console.WriteLine("\n-----------");
+
+            lab.CategoryCount();
+
+
+            Console.WriteLine("-----------\nnew list:");
+
+            lab.NewListValues = new List<Products>() { new Device { Name = "new device", Price = 999, Manufacturer = "рб", Category = ProductCategorty.Device } };
+            lab.Print();
+
+        }
+    }
+}
---
+++ 5/Partial.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    public partial class Printer
+    {
+        public static void IAmPrintingPartial(Products products)
+        {
+            Console.WriteLine("Partial Printer:");
+            Console.WriteLine("    " + products.ToString() + "\n");
+        }
+    }
+
+    public partial class Laboratory // ???????????? get и set методы конструктора
+    {
+        private List<Products> allProducts;
+        // ______________ Конструктор ______________
+        public Laboratory()
+        {
+            allProducts = new List<Products>();
+        }
+
+        // ______________ Функции ______________
+        public void Add(Products products)
+        {
+            allProducts.Add(products);
+        }
+        public void Remove(Products products)
+        {
+            allProducts.Remove(products);
+        }
+
+        public void SortByPrice()
+        {
+            List<Products> sortAllProducts = new(allProducts.OrderByDescending(p => p.Price)) ;
+            Console.Write("sort by price: \n");
+            foreach (Products tech in sortAllProducts)
+            {
+                Console.WriteLine("    " + tech);
+            }
+        }
+
+        public void CategoryCount()
+        {
+            int allCount = 0;
+
+            Dictionary<ProductCategorty, int> categoryCount = new();
+
+            //int deviceCount = 0;
+            //int printerCount = 0;
+            //int scannerCount = 0;  
+            //int computerCount = 0;
+            //int tabletCount = 0;
+
+            foreach (Products product in allProducts)
+            {
+                allCount++;
+                if(categoryCount.ContainsKey(product.Category))
+                {
+                    categoryCount[product.Category]++;
+                }
+                else
+                {
+                    categoryCount.Add(product.Category, 1);
+                }
+            }
+            foreach(KeyValuePair<ProductCategorty, int> item in categoryCount)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+
+        public List<Products> NewListValues
+        {
+            get { return allProducts; }
+            set { allProducts = value; }
+        }
+
+        public void Print()
+        {
+            foreach (Products product in allProducts)
+            {
+                Console.WriteLine(product.ToString());
+            }
+        }
+    }
+}
